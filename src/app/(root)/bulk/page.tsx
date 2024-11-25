ac79020db6c2983cd7ff4abfb62aada7add2af08
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2024-11-25 18:02:26 +0530

    Updt

--- src/app/(root)/bulk/page.tsx
+++ src/app/(root)/bulk/page.tsx
@@ -18,7 +18,12 @@ function page() {
           const workbook = XLSX.read(data, { type: 'array' });
           const firstSheetName = workbook.SheetNames[0];
           const worksheet = workbook.Sheets[firstSheetName];
+        //   console.log(worksheet);
+          
           const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+             // Process the data to replace blank valu
+
           setProducts(jsonData);
         };
         reader.readAsArrayBuffer(file);
@@ -29,41 +34,66 @@ function page() {
 
     const [data, setData] = useState<Matrix<CellBase<any>>>([
         [
+            {
+                value: "Marks",
+                readOnly: true,
+                className: "text-danger"
+              },
           {
-            value: "redOnly + text-color",
+            value: "Question",
             readOnly: true,
             className: "text-danger"
           },
-          { value: "text-color", className: "text-danger" },
-          { value: "readOnly", readOnly: true },
-          { value: "readOnly + css", readOnly: true, className: "header-row" },
-          { value: "css", className: "header-row" },
-          { value: "no options" }
-        ],
-        [
-          { value: "Strawberry" },
-          { value: "Cookies" },
-          { value: "Vanilla" },
-          { value: "Chocolate" },
-          { value: "Citrus" },
-          { value: "Green Apple" }
+          {
+            value: "Answer",
+            readOnly: true,
+            className: "text-danger"
+          },
+          { value: "option_1",readOnly: true, className: "text-danger" },
+          { value: "option_2", readOnly: true },
+          { value: "option_3", readOnly: true, className: "header-row" },
+          { value: "option_4", readOnly: true },
+          { value: "option_5", readOnly: true, className: "header-row" },
+          { value: "option_6", readOnly: true, className: "header-row" },
+         
         ]
       ]);
-    
+
+      // Remove duplicates based on 'Name'
+      const uniqueData = products.filter((value, index, self) => 
+        index === self.findIndex((t) => t.Name === value.Name)
+      );
+      const dataPost = uniqueData
+      
       const arrayOfArrays = [
         // Add the header (keys) as the first row
        
         
         // Add the data rows
-        ...products.map(item => (Object.values(item).map(el=>({'value':el}))))
+        ...uniqueData.map(item => (Object.entries(item).map(([key,val])=>({'value':item[key]}))))
       ];
 
-    const onSubmit =()=>{
+      
+      
+
+    const onSubmit =async()=>{
         console.log(fileName);
         
-        console.log(products);
-        console.log(arrayOfArrays);
+
+       
+        //   console.log(uniqueData);
+
+       
+        
         setData([...data,...arrayOfArrays])
+        dataPost.map(el=>((el.Chemistry == 'NA'&& delete el.Chemistry)|| (el.Physics == 'NA'&& delete el.Physics)))
+        console.log(products);
+        console.log(dataPost);
+        
+        console.log(uniqueData);
+        
+        console.log(dataPost);
+        
     }
 
 
